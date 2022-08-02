9b427666245ac030fe3ae9bba594025628bd753e
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-08-02 11:29:22 -0500

    controller exercise start

---
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {return "<h1>This is the landing page!</h1>";}
+}
