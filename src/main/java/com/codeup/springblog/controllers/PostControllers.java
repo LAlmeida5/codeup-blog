49909f2d603aab0c23fd38d81628106902aa5f10
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-08-04 11:13:15 -0500

    update

--- src/main/java/com/codeup/springblog/controllers/PostControllers.java
+++ src/main/java/com/codeup/springblog/controllers/PostControllers.java
@@ -1,11 +1,16 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostControllers {
@@ -17,26 +22,25 @@ public class PostControllers {
     }
 
 
-    @PostMapping("/posts")
-    @ResponseBody
-    public String indexPage(){
-        return "<h1>post index page</h1>";
+    @GetMapping("/posts")
+    public String indexPage(Model model){
+
+        model.addAttribute("posts", postDao.findAll());
+
+        return "posts/index";
     }
 
     @PostMapping("/posts/{id}")
-    @ResponseBody
     public String indPost(@PathVariable int id){
         return "<h1>view an individual post</h1>";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
     public String getPage(){
         return "<h1>view the form for creating a post</h1>";
     }
 
     @PostMapping("/posts/create")
-    @ResponseBody
     public String postPage(){
         return "<h1>create a new post</h1>";
     }
